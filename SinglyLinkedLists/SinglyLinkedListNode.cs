a79696d29411cf2938b9e5e44709f37cd3c8b6f5
rob1nburt0n <1.robinburton@gmail.com>
2015-10-27 10:45:45 -0500

    complete first assignment

--- SinglyLinkedLists/SinglyLinkedListNode.cs
+++ SinglyLinkedLists/SinglyLinkedListNode.cs
@@ -15,14 +15,24 @@ namespace SinglyLinkedLists
         private SinglyLinkedListNode next;
         public SinglyLinkedListNode Next
         {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get
+            {
+                return next;
+            }
+            set
+            {
+                if (this == value)
+                {
+                    throw new ArgumentException();
+                }
+                next = value;
+            }
         }
 
         private string value;
         public string Value 
         {
-            get { throw new NotImplementedException(); }
+            get { return value;  }
         }
 
         public static bool operator <(SinglyLinkedListNode node1, SinglyLinkedListNode node2)
@@ -39,10 +49,14 @@ namespace SinglyLinkedLists
 
         public SinglyLinkedListNode(string value)
         {
-            throw new NotImplementedException();
-
+           this.value = value;
             // Used by the visualizer:
             allNodes.Add(this);
+        }
+
+        public override string ToString()
+        {
+            return value;
         }
 
         // READ: http://msdn.microsoft.com/en-us/library/system.icomparable.compareto.aspx
